770c2cb86662531e2000f99beabadee20ebb71e3
Jahn Thomas Fidje <jtfidje@gmail.com>
2021-12-09 07:59:28 +0100

    Add cleaned Python solution for Day 9

---
+++ 2021/python/day_09/day_09/cleaned.py
@@ -0,0 +1,109 @@
+import math
+from typing import Dict, List, Tuple
+
+
+def read_lines(path: str) -> List[str]:
+    with open(path, "r") as f:
+        lines = [line for line in f.readlines()]
+        lines = [line.strip() for line in lines]
+        lines = [[int(x) for x in line] for line in lines]
+        return lines
+
+
+def check_low_point(i: int, j: int, height_map: List[List[int]]) -> bool:
+    neighbours = []
+
+    if i > 0:
+        # Above
+        neighbours.append(height_map[i - 1][j])
+
+    if i < len(height_map) - 1:
+        # Below
+        neighbours.append(height_map[i + 1][j])
+
+    if j > 0:
+        # Left
+        neighbours.append(height_map[i][j - 1])
+
+    if j < len(height_map[0]) - 1:
+        # Right
+        neighbours.append(height_map[i][j + 1])
+
+    loc = height_map[i][j]
+    return all(loc < x for x in neighbours)
+
+
+def calculate_basin_size(
+    i: int, j: int, height_map: List[List[int]], visited: Dict[Tuple[int], bool]
+) -> int:
+    count = 0
+    visited[(i, j)] = True
+    if height_map[i][j] == 9:
+        return count
+
+    if i > 0:
+        # Above
+        n_i, n_j = i - 1, j
+        if (n_i, n_j) not in visited:
+            count += calculate_basin_size(n_i, n_j, height_map, visited)
+
+    if i < len(height_map) - 1:
+        # Below
+        n_i, n_j = i + 1, j
+        if (n_i, n_j) not in visited:
+            count += calculate_basin_size(n_i, n_j, height_map, visited)
+
+    if j > 0:
+        # Left
+        n_i, n_j = i, j - 1
+        if (n_i, n_j) not in visited:
+            count += calculate_basin_size(n_i, n_j, height_map, visited)
+
+    if j < len(height_map[0]) - 1:
+        # Right
+        n_i, n_j = i, j + 1
+        if (n_i, n_j) not in visited:
+            count += calculate_basin_size(n_i, n_j, height_map, visited)
+
+    return count + 1
+
+
+def solve_1(path: str) -> int:
+    height_map = read_lines(path)
+    risk_lvl_sum = 0
+    for i, row in enumerate(height_map):
+        for j, col in enumerate(row):
+            if check_low_point(i, j, height_map):
+                risk_lvl_sum += col + 1
+
+    return risk_lvl_sum
+
+
+def solve_2(path: str) -> int:
+    height_map = read_lines(path)
+    basin_sizes = []
+    visited = {}
+    for i, row in enumerate(height_map):
+        for j, col in enumerate(row):
+            if (i, j) not in visited:
+                size = calculate_basin_size(i, j, height_map, visited)
+                basin_sizes.append(size)
+
+    basin_sizes = sorted(basin_sizes)
+    return math.prod(basin_sizes[-3:])
+
+
+if __name__ == "__main__":
+    path = "example.txt"
+    ans_1_example = solve_1(path)
+    ans_2_example = solve_2(path)
+
+    path = "input.txt"
+    ans_1_input = solve_1(path)
+    ans_2_input = solve_2(path)
+
+    print(f"Example 1: {ans_1_example}")
+    print(f"Example 2: {ans_2_example}")
+    print("\n- - -\n")
+    print(f"Problem 1: {ans_1_input}")
+    print(f"Problem 2: {ans_2_input}")
--- 2021/python/day_09/tests/test_day_09.py
+++ 2021/python/day_09/tests/test_day_09.py
@@ -17,17 +17,17 @@ def test_solve_2_run_input():
     assert run.solve_2("input.txt") == 821560
 
 
-#def test_solve_1_cleaned_example():
-#    assert cleaned.solve_1("input.txt") == None
-#
-#
-#def test_solve_2_cleaned_example():
-#    assert cleaned.solve_2("example.txt") == None
-#
-#
-#def test_solve_1_cleaned_input():
-#    assert cleaned.solve_1("input.txt") == None
-#
-#
-#def test_solve_2_cleaned_input():
-#    assert cleaned.solve_2("input.txt") == None
+def test_solve_1_cleaned_example():
+    assert cleaned.solve_1("example.txt") == 15
+
+
+def test_solve_2_cleaned_example():
+    assert cleaned.solve_2("example.txt") == 1134
+
+
+def test_solve_1_cleaned_input():
+    assert cleaned.solve_1("input.txt") == 631
+
+
+def test_solve_2_cleaned_input():
+    assert cleaned.solve_2("input.txt") == 821560
