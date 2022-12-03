e0cded67b23c02cf1c1dd8e9857202b441a6312d
Jahn Thomas Fidje <jtfidje@gmail.com>
2022-12-03 17:21:01 +0100

    fix: Fixed failing tests for day_01 cleaned solution

--- 2022/python/day_01/day_01/cleaned.py
+++ 2022/python/day_01/day_01/cleaned.py
@@ -9,21 +9,22 @@ def read_data(path: str) -> list[list[int]]:
     return data
 
 
-def solve_1(data: list[list[int]]) -> int:
+def solve_1(path: str) -> int:
+    data = read_data(path)
+
     return reduce(max, map(sum, data))
 
 
-def solve_2(data: list[list[int]]) -> int:
+def solve_2(path: str) -> int:
+    data = read_data(path)
+
     data = sorted(map(sum, data))
     return sum(data[-3:])
 
 
 if __name__ == "__main__":
-    example_data = read_data("example.txt")
-    input_data = read_data("input.txt")
-
-    print(f"Example 1: {solve_1(example_data)}")
-    print(f"Example 2: {solve_2(example_data)}")
+    print(f"Example 1: {solve_1('example.txt')}")
+    print(f"Example 2: {solve_2('example.txt')}")
     print("\n- - -\n")
-    print(f"Problem 1: {solve_1(input_data)}")
-    print(f"Problem 2: {solve_2(input_data)}")
+    print(f"Problem 1: {solve_1('input.txt')}")
+    print(f"Problem 2: {solve_2('input.txt')}")
