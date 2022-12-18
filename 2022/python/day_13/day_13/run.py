ba45b2812a81d8fe3455c9279a2f46c44e78c42c
Jahn Thomas Fidje <jtfidje@gmail.com>
2022-12-18 12:10:16 +0100

    chore: Add solution for Day 13

--- 2022/python/day_13/day_13/run.py
+++ 2022/python/day_13/day_13/run.py
@@ -1,6 +1,7 @@
 import json
 from functools import reduce
 from typing import Any
+from functools import cmp_to_key
 
 def json_print(obj: dict | list) -> None:
     print(json.dumps(obj, indent=4))
@@ -27,6 +28,15 @@ def read_data(path: str) -> list[str]:
 
         return pairs
 
+def read_packets(path: str) -> list[str]:
+    packets = []
+    with open(path) as f:
+        data = f.read().strip().replace("\n\n", "\n")
+        for packet in data.split("\n"):
+            packet.strip()
+            packets.append(json.loads(packet))
+    return packets
+
 
 def read_numbers(path: str) -> list[int]:
     with open(path, "r") as f:
@@ -36,35 +46,34 @@ def read_numbers(path: str) -> list[int]:
 
 
 def compare(left, right):
-    print(left)
-    print(right)
-    print("-")
+    left = left.copy()
+    right = right.copy()
     while True:
         if left == [] and right == []:
-            return None
+            return 0
         try:
             l = left.pop(0)
         except IndexError:
-            return True
+            return 1
 
         try:
             r = right.pop(0)
         except IndexError:
-            return False
+            return -1
         
         if all([isinstance(l, int), isinstance(r, int)]):
             if l > r:
-                return False
+                return -1
 
             elif l < r:
-                return True
+                return 1
             
             else: 
                 continue
         
         elif all([isinstance(l, list), isinstance(r, list)]):
             res = compare(l, r)
-            if res is None:
+            if res == 0:
                 continue
             return res
         
@@ -75,7 +84,7 @@ def compare(left, right):
                 r = [r]
             
             res = compare(l, r)
-            if res is None:
+            if res == 0:
                 continue
             return res
 
@@ -85,22 +94,39 @@ def solve_1(path: str) -> Any:
     indices = []
     for i, (left, right) in enumerate(pairs, start=1):
 
-        if compare(left, right):
-            print("True")
+        _cmp = compare(left, right)
+        if _cmp == 1:
             indices.append(i)
-        else:
-            print("False")
-        print()
-
     return sum(indices)
                 
 
+def packet_string(packet):
+    if packet == []:
+        return "0"
+    
+    string = ""
+
+    for value in packet:
+        if isinstance(value, list):
+            s = packet_string(value)
+        else:
+            s = str(value)
+
+        string += s
+            
+    return string
+
+
 
 def solve_2(path: str) -> Any:
-    data = read_lines(path)
-    ...
+    packets = read_packets(path)
+    packets.extend([[[2]], [[6]]])
 
-
+    packets = sorted(packets, key=cmp_to_key(compare), reverse=True)
+    i_1 = packets.index([[2]])
+    i_2 = packets.index([[6]])
+    return (i_1 + 1) * (i_2 + 1)
+    
 if __name__ == "__main__":
     print(f"Example 1: {solve_1('example.txt')}")
     print(f"Example 2: {solve_2('example.txt')}")
--- 2022/python/day_13/tests/test_day_13.py
+++ 2022/python/day_13/tests/test_day_13.py
@@ -9,24 +9,24 @@ def test_solve_2_run_example():
     assert run.solve_2("example.txt") == 140
 
 
-#def test_solve_1_run_input():
-#    assert run.solve_1("input.txt") == None
-#
-#
+def test_solve_1_run_input():
+    assert run.solve_1("input.txt") == 6656
+
+
 #def test_solve_2_run_input():
 #    assert run.solve_2("input.txt") == None
 #
 #
 #def test_solve_1_cleaned_example():
-#    assert cleaned.solve_1("example.txt") == None
+#    assert cleaned.solve_1("example.txt") == 13
 #
 #
 #def test_solve_2_cleaned_example():
-#    assert cleaned.solve_2("example.txt") == None
+#    assert cleaned.solve_2("example.txt") == 140
 #
 #
 #def test_solve_1_cleaned_input():
-#    assert cleaned.solve_1("input.txt") == None
+#    assert cleaned.solve_1("input.txt") == 6656
 #
 #
 #def test_solve_2_cleaned_input():
