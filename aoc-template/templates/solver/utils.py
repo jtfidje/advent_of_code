ae63f168b4e24040cc0b8426672d145cd710ed91
Jahn Thomas Fidje <jtfidje@gmail.com>
2023-12-04 19:53:50 +0100

    chore: Add util function to template for fetching adjacent points around one or multiple points in a 2d-array

--- aoc-template/templates/solver/utils.py
+++ aoc-template/templates/solver/utils.py
@@ -27,3 +27,31 @@ def sliding_window(
         step = window
     for i in range(0, len(array) - window + 1, step):
         yield array[i : i + window]
+
+
+def out_of_bounds(row: int, col: int, matrix: list[list]):
+    if row < 0 or row >= len(matrix):
+        return True
+    
+    if col < 0 or col >= len(matrix[0]):
+        return True
+    
+    return False
+
+
+def get_adjacent(row: int, col: int, matrix: list[list], width: int = 1, height: int = 1) -> tuple[int, int]:
+    skip_positions = [(row + i, col + j) for i in range(height) for j in range(width)]
+    adjacent = []
+    for i in range(row - 1, row + 1 + height):
+        for j in range(col - 1, col + 1 + width):
+            # Skip current position
+            if (i, j) in skip_positions:
+                continue
+
+            # Check if out of bounds
+            if out_of_bounds(row, col, matrix):
+                continue
+            
+            adjacent.append((i, j))
+    
+    return adjacent
