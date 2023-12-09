8303842d754da6f9a1702f312037c35c5192094c
Jahn Thomas Fidje <jtfidje@gmail.com>
2023-12-09 15:03:54 +0100

    chore: Day 08 checkpoint

--- 2023/python/day-08/solver/part_2.py
+++ 2023/python/day-08/solver/part_2.py
@@ -1,19 +1,30 @@
-# flake8: noqa: F401
+from __future__ import annotations
 
+import multiprocessing
+import time
+from enum import StrEnum
 from pathlib import Path
+from typing import Self
 
 from solver import utils
 
 data_path = Path(__file__).parent.parent.absolute() / "data"
 
 
+class WorkerState(StrEnum):
+    pending = "pending"
+    running = "running"
+    halted = "halted"
+    finished = "finished"
+
+
 class Node:
-    def __init__(self, name: str, left: str, right: str):
+    def __init__(self, name: str, left: Self | None, right: Self | None):
         self.name = name
         self.left = left
         self.right = right
 
-    def get_neighbour(self, dir: str):
+    def get_neighbour(self, dir: str) -> Self:
         return self.left if dir == "L" else self.right
     
     def is_terminal(self) -> bool:
@@ -21,6 +32,34 @@ class Node:
 
     def __repr__(self):
         return f"{self.left} <-- {self.name} --> {self.right}"
+
+
+def work(
+        worker_id: str,
+        node: Node,
+        instructions: str,
+        to_manager_queue: multiprocessing.Queue,
+        from_manager_queue: multiprocessing.Queue):
+    print(worker_id, "started!")
+    current_state = WorkerState.running
+    index = 0
+    while True:
+        if current_state == WorkerState.halted:
+            continue
+
+        if node.is_terminal():
+            to_manager_queue.put(index)
+            
+            new_state = from_manager_queue.get()
+            if new_state == WorkerState.finished:
+                break
+
+        i = index % len(instructions)
+        instruction = instructions[i]
+        node = node.get_neighbour(instruction)
+
+        index += 1
+
 
 def solve(path: str):
     data = utils.read_lines(path)
@@ -33,24 +72,56 @@ def solve(path: str):
         name, dirs = line.split(" = ")
         left, right = dirs[1:-1].split(", ")
 
-        node_map[name] = Node(name, left, right)
+        node_map[name] = Node(name, None, None)
+
+    for line in data:
+        name, dirs = line.split(" = ")
+        left, right = dirs[1: -1].split(", ")
+
+        node = node_map[name]
+        node.left = node_map[left]
+        node.right = node_map[right]
 
     nodes = [node for node in node_map.values() if node.name.endswith("A")]
-    counter = 0
-    while True:
-        i = counter % len(instructions)
-        instruction = instructions[i]
+    with multiprocessing.Manager() as manager:
+        workers: list[dict] = []
+        for i, node in enumerate(nodes):
+            to_manager_queue = manager.Queue()
+            from_manager_queue = manager.Queue()
+            worker_id = f"Worker-{i}"
         
-        counter += 1
+            process = multiprocessing.Process(
+                target=work,
+                args=(worker_id, node, instructions, to_manager_queue, from_manager_queue)
+            )
 
-        for _ in range(len(nodes)):
-            node = nodes.pop(0)
-            nodes.append(node_map[node.get_neighbour(instruction)])
+            workers.append({
+                "name": worker_id,
+                "to_manager_queue": to_manager_queue,
+                "from_manager_queue": from_manager_queue,
+                "process": process
+            })
+            process.start()
 
-        if all(node.is_terminal() for node in nodes):
-            break
+        index = None
+        print("Main process entering loop...")
+        while True:
+            indices = set()
+            for worker in workers:
+                index = worker["to_manager_queue"].get()
+                indices.add(index)
+
+            if len(indices) == 1:
+                for worker in workers:
+                    worker["from_manager_queue"].put(WorkerState.finished)
+                    worker["process"].join()
+                index = indices[0]
+                break
+            
+            for worker in workers:
+                worker["from_manager_queue"].put(WorkerState.running)
     
-    return counter
+    return index
 
 
 if __name__ == "__main__":
