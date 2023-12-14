0373acd308d751d12b73db911a0677478f1f090d
Jahn Thomas Fidje <jtfidje@gmail.com>
2023-12-14 22:02:45 +0100

    chore: Part 2 checkpoint

--- day_14/solver/part_2.py
+++ day_14/solver/part_2.py
@@ -1,5 +1,5 @@
 # flake8: noqa: F401
-
+from collections import defaultdict
 from pathlib import Path
 
 from solver import utils
@@ -7,8 +7,38 @@ from solver import utils
 data_path = Path(__file__).parent.parent.absolute() / "data"
 
 
+
 def solve(path: str):
     data = utils.read_lines(path)
+    data = [list(line) for line in data]
+
+    for line in data:
+        index = -1
+        count = 0
+        print("".join(line))
+        for col, char in enumerate(line[:]):
+            match char:
+                case ".":
+                    continue
+                case "#":
+                    if count == 0:
+                        index = col
+                        continue
+
+                    new_line = (["O"] * count) + ["."] * ((col - (index + 1)) - count)
+        
+                    line[index + 1: col] = new_line
+                    index = col
+                    count = 0
+                case "O":
+                    count += 1
+
+        print("".join(line))
+
+    results = 0
+    return results
+
+            
 
 
 if __name__ == "__main__":
